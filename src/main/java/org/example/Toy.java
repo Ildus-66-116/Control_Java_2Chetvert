c04a1fd89e18dad6880365443138f5ae44e75cb6
ILDUS <ildus6696@gmail.com>
2023-11-10 15:59:48 +0300

    DZ2

---
+++ src/main/java/org/example/Toy.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public class Toy {
+    private int id;
+    private String name;
+    private int rate;
+
+    public Toy(int id,  int rate, String name ) {
+        this.id = id;
+        this.name = name;
+        this.rate = rate;
+    }
+    public int getRate() {
+        return rate;
+    }
+
+    @Override
+    public String toString() {
+        return  id + " " + rate + " " + name;
+    }
+}
